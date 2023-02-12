2612c24cb4e3ff58cafd70b8e8a1ed3edc3a76f5
its_DC <96367023+Devottam2809@users.noreply.github.com>
2023-02-12 09:08:09 +0530

    Add files via upload

---
+++ Mini_GUI/gguuii.py
@@ -0,0 +1,11 @@
+from tkinter import*
+
+win=Tk()
+win.geometry("10x10")
+win.config(background="black")
+win.wm_attributes('-alpha', 0.5)
+win.wm_attributes('-fullscreen', 'True')
+win.wm_attributes('-topmost', True)
+
+
+win.mainloop()
