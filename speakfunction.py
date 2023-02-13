14b218833bccbb6a0b2d37df25bb301a7e2b5956
its_DC <96367023+Devottam2809@users.noreply.github.com>
2023-02-13 21:44:38 +0530

    Add files via upload

---
+++ speakfunction.py
@@ -0,0 +1,33 @@
+import pyttsx3
+import speech_recognition as sr
+
+Assistant = pyttsx3.init('sapi5')
+voices = Assistant.getProperty('voices')
+Assistant.setProperty('voice', voices[2].id)
+Assistant.setProperty('rate',170)
+
+def speak(audio):
+    print("  ")
+    Assistant.say(audio)
+    print("  ")
+    print(f" : {audio}")
+
+    Assistant.runAndWait()
+
+def takecommand():
+    command = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("listening.....")
+        command.pause_threshold = 1
+        audio = command.listen(source)
+
+        try:
+            print("Recognizing....")
+            query = command.recognize_google(audio,language='en-in')
+            print(f"You said : {query}")
+
+        except Exception as Error:
+            return "none"
+        
+        return query.lower()
+
