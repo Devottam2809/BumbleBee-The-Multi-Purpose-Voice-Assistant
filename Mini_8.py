0cab8d20a06a1f8ed89d001214b8b995b3a4c413
its_DC <96367023+Devottam2809@users.noreply.github.com>
2023-02-28 18:47:41 +0530

    Add files via upload

---
+++ Mini_8.py
@@ -0,0 +1,70 @@
+import pyttsx3
+import speech_recognition as sr
+from datetime import datetime
+
+Assistant = pyttsx3.init('sapi5')
+voices = Assistant.getProperty('voices')
+Assistant.setProperty('voice', voices[2].id)
+Assistant.setProperty('rate',170)
+
+def speak(audio):
+    print("  ")
+    Assistant.say(audio)
+    print("  ")
+    print(f" : {audio}")
+
+    Assistant.runAndWait()
+
+def takecommand():
+    command = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("listening.....")
+        command.pause_threshold = 1
+        audio = command.listen(source)
+
+        try:
+            print("Recognizing....")
+            query = command.recognize_google(audio,language='en-in')
+            print(f"You said : {query}")
+
+        except Exception as Error:
+            return "none"
+        
+        return query.lower()
+
+one='''
+In this time ,you have to workout.
+'''
+
+two='''
+In this time you have to study Maths, because the CT marks are just disgusting.
+'''
+
+three='''
+Time to code.
+Make a post on Github.
+
+Check your linkedin
+'''
+
+four='''
+In this time you have to study.
+'''
+def time():
+    hour=int(datetime.now().strftime("%H"))
+    if hour>6  and hour<=7:
+        speak(f'In this time ,you have to workout.')
+
+    elif hour>=9 and hour<=12:
+        speak(two)
+    
+    elif hour>=16 and hour<=18:
+        speak(three)
+
+    elif hour >= 20 and hour<=25:
+        speak(four)
+    else:
+        speak("Take Rest")
+
+
+
